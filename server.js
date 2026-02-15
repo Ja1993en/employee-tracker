75623d1d536585d20187d909ee8e6b1565b6bbd7
Basquiat <jlnmcneal@gmail.com>
2026-02-14 20:59:47 -0600

    Function to add department

--- server.js
+++ server.js
@@ -92,4 +92,62 @@ const db = mysql.createConnection(
       startApp()
     });
 }
+
+const addRole = async () => {
+
+  db.query(`SELECT * FROM department`, function (err, result, fields) {
+    if (err) throw err;
+  
+    console.log(result);
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'role',
+          message: `What is the name of the role`,
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: 'What is the salary of the role ',
+        },
+        {
+          type: 'rawlist',
+          name: 'department',
+          message: 'What department does the role belong to ',
+          choices: result.map(arr => arr.name),
+        }
+      ])
+      .then((answer) => {
+        const founddpt = result.find(dpt => dpt.name === answer.department)
+
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${answer.role}",  ${answer.salary}, ${founddpt.id} );`, function (err, result, fields) {
+          if (err) throw err;
+          // console.log(table)/
+          startApp()
+        })
+      })
+  })
+}
+
+// Function to add department to db
+const addDepartment = async () => {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'department',
+          message: 'What is the department name ',
+        }
+      ])
+      .then((res) => {
+        
+  
+        db.query(`INSERT INTO department (name) VALUES ("${res.department}")`, function (err, result, fields) {
+          if (err) throw err;
+          startApp()
+        })
+      })
+  }
+
   startApp();
